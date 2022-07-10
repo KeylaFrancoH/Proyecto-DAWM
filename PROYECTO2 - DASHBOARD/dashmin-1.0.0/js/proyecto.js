d93b997dae8db65a3cbc24071244e52f45cb762a
Keyla Franco Hidalgo <kefrhida@espol.edu.ec>
2022-07-09 22:45:53 -0500

    avance

--- PROYECTO2 - DASHBOARD/dashmin-1.0.0/js/proyecto.js
+++ PROYECTO2 - DASHBOARD/dashmin-1.0.0/js/proyecto.js
@@ -4,29 +4,27 @@ var selecc = document.getElementById('select-primary')
 let URL = 'https://ghibliapi.herokuapp.com/films/'
 
 
-fetch(URL)
+function mostrarDatos() {
+    fetch(URL)
     .then(respuesta => respuesta.json())
     .then(datos => {
-        mostrarDatos(datos);
-        seleccionar_film(datos);
+        let body = ""
+        for (var i = 0; i < datos.length; i++) {      
+           body+=`<tr>
+           <th scope="col"><input class="form-check-input" type="checkbox"></th>
+           <th scope ="col">${datos[i].release_date}</th>
+           <th scope="col">${datos[i].title}</th>
+           <th scope="col">${datos[i].original_title}</th>
+           <th scope="col">${datos[i].producer}</th>
+           <th scope="col">${datos[i].director}</th>
+           <th scope="col">${datos[i].rt_score}</th>
+       </tr>`
+        }
+        contenido.innerHTML = body
+        seleccionar_film(datos)
     })
     .catch(console.error);
-
-
-function mostrarDatos(datos) {
-    let body = ""
-            for (var i = 0; i < datos.length; i++) {      
-               body+=`<tr>
-               <th scope="col"><input class="form-check-input" type="checkbox"></th>
-               <th scope ="col">${datos[i].release_date}</th>
-               <th scope="col">${datos[i].title}</th>
-               <th scope="col">${datos[i].original_title}</th>
-               <th scope="col">${datos[i].producer}</th>
-               <th scope="col">${datos[i].director}</th>
-               <th scope="col">${datos[i].rt_score}</th>
-           </tr>`
-            }
-            contenido.innerHTML = body
+   
 }
 
 function seleccionar_film(datos){
@@ -36,3 +34,7 @@ function seleccionar_film(datos){
       }
       selecc.innerHTML = titulos
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    mostrarDatos()
+});
