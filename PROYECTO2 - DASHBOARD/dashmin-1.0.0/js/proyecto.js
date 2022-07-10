9e68035666faf668d8a781edc405a39d9a73c705
Keyla Franco Hidalgo <kefrhida@espol.edu.ec>
2022-07-09 23:49:29 -0500

    avance

--- PROYECTO2 - DASHBOARD/dashmin-1.0.0/js/proyecto.js
+++ PROYECTO2 - DASHBOARD/dashmin-1.0.0/js/proyecto.js
@@ -1,16 +1,24 @@
 
 var contenido = document.querySelector('#contenido')
 var selecc = document.getElementById('select-primary')
+var selector_foto = document.getElementById('portada')
+var selector = document.querySelector('#bodycard')
 let URL = 'https://ghibliapi.herokuapp.com/films/'
 
-
-function mostrarDatos() {
-    fetch(URL)
+fetch(URL)
     .then(respuesta => respuesta.json())
     .then(datos => {
-        let body = ""
-        for (var i = 0; i < datos.length; i++) {      
-           body+=`<tr>
+        mostrarDatos(datos)
+        seleccionar_film(datos)
+    })
+    .catch(console.error);
+
+
+function mostrarDatos(datos) {
+
+    let body = ""
+    for (var i = 0; i < datos.length; i++) {
+        body += `<tr>
            <th scope="col"><input class="form-check-input" type="checkbox"></th>
            <th scope ="col">${datos[i].release_date}</th>
            <th scope="col">${datos[i].title}</th>
@@ -19,22 +27,18 @@ function mostrarDatos() {
            <th scope="col">${datos[i].director}</th>
            <th scope="col">${datos[i].rt_score}</th>
        </tr>`
-        }
-        contenido.innerHTML = body
-        seleccionar_film(datos)
-    })
-    .catch(console.error);
-   
+    }
+    contenido.innerHTML = body
+
 }
 
-function seleccionar_film(datos){
+function seleccionar_film(datos) {
     let titulos = "<option selected>Seleccione el nombre de una película</option>"
-      for (valor of datos) {
-            titulos+=` <option value="">${valor['title']}</option> `
-      }
-      selecc.innerHTML = titulos
-}
+    for (valor of datos) {
+        titulos += ` <option value="">${valor['title']}</option> `
+    }
+    selecc.innerHTML = titulos
+    selector_foto.src = `${valor['image']}`
+    selector.textContent = `${valor['description']}`
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    mostrarDatos()
-});
+}
