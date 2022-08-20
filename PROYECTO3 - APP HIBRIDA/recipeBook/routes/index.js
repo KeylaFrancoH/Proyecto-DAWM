c600c928bf2965f16882237e48da4f8df09aade2
Keyla Franco Hidalgo <kefrhida@espol.edu.ec>
2022-08-19 23:29:48 -0500

    cors

--- PROYECTO3 - APP HIBRIDA/recipeBook/routes/index.js
+++ PROYECTO3 - APP HIBRIDA/recipeBook/routes/index.js
@@ -10,36 +10,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/ingredientes', function(req, res, next) {
-  models.ingredientes.findAll({ 
-  
-   
-  })
- 
- .then(ingredients => {
-    res.json(ingredients)
- })
- .catch(error => res.status(400).send(error))
-});
-
-router.get('/recetas', function(req, res, next) {
-  models.recetas.findAll({
-    
-  })
- .then(recipe => {
-    res.json(recipe)
- })
- .catch(error => res.status(400).send(error))
-});
-
-router.get('/usuarios', function(req, res, next) {
-  models.usuarios.findAll({
-    
-  })
- .then(user => {
-    res.json(user)
- })
- .catch(error => res.status(400).send(error))
-});
-
 module.exports = router;
